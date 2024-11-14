453f15fd6e8356026e1199e85bb830e8c8d14c10
ksk <133188669+ksk-117@users.noreply.github.com>
2024-11-14 10:34:21 +0900

    TodoListコンポーネントに優先度を星で表示する機能を追加し、スタイルを調整

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { Todo } from "./types";
+import dayjs from "dayjs";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFile,
+  faClock,
+  faFaceGrinWide,
+} from "@fortawesome/free-solid-svg-icons";
+import { twMerge } from "tailwind-merge";
 
 type Props = {
   todos: Todo[];
 };
+
+const num2star = (n: number): string => "★".repeat(4 - n);
 
 const TodoList = (props: Props) => {
   const todos = props.todos;
@@ -17,10 +27,15 @@ const TodoList = (props: Props) => {
   }
 
   return (
-    <div className="space-y-1">
+    <div className="space-y-2">
       {todos.map((todo) => (
         <div key={todo.id}>
-          {todo.name} 優先度: {todo.priority}
+          <div>
+            {todo.name} 優先度: {todo.priority}
+          </div>
+          <div className="ml-2 text-orange-400">
+            {num2star(todo.priority)}
+          </div>
         </div>
       ))}
     </div>
