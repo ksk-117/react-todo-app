87e8db3b602385fcde9466eb486645f7c447eb82
ksk <133188669+ksk-117@users.noreply.github.com>
2024-11-14 09:46:02 +0900

    Tailwind CSSとPostCSSの設定を追加し、スタイルを適用

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,14 @@ const App = () => {
     setCount(newCount);
   };
   return (
-    <div>
-      <h1>TodoApp</h1>
-      <button onClick={countUp}>count is {count}</button>
+    <div className="mx-4 md:mx-auto mt-10 max-w-2xl">
+      <h1 className="font-bold text-2xl mb-4">TodoApp</h1>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-md"
+        onClick={countUp}
+      >
+        count is {count}
+      </button>
     </div>
   );
 };
